077415aaaa5f3e0be706f7d1fb1d08e962a147d9
DaymianDark <nightkonn@gmail.com>
2022-02-01 14:27:42 +0300

    lesson 11 comment added

--- src/test/java/com/berezkindv/tests/TestData.java
+++ src/test/java/com/berezkindv/tests/TestData.java
@@ -5,4 +5,5 @@ public class TestData {
     public static String yaSearch = "https://yandex.ru/";
     public static String gooSearch = "https://google.com/";
     public static String bingSearch = "https://www.bing.com/";
+    // lesson 11 homework changes
 }
