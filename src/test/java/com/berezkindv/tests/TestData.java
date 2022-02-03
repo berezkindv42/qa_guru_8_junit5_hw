64f81707e2389dfa35f51e104bcc7115a9ecf07e
DaymianDark <nightkonn@gmail.com>
2022-02-03 12:45:30 +0300

    comment for reviewer deleted

--- src/test/java/com/berezkindv/tests/TestData.java
+++ src/test/java/com/berezkindv/tests/TestData.java
@@ -6,5 +6,3 @@ public class TestData {
     public static String gooSearch = "https://google.com/";
     public static String bingSearch = "https://www.bing.com/";
 }
-
-/* этот комментарий добавлен для создания тестового пул реквеста в гитхабе и проверки его ревьювером */
