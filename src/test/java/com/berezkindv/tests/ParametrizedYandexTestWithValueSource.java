7aeebdb0abda030c437b095b0964750d58f13335
SvetlanaVaskevich <81557049+SvetlanaVaskevich@users.noreply.github.com>
2022-02-03 14:19:28 +0500

    Second branch (#4)
    * Commit added in ParametrizedYandexTestWithValueSource
    
    * New commit added in ParametrizedYandexTestWithValueSource

--- src/test/java/com/berezkindv/tests/ParametrizedYandexTestWithValueSource.java
+++ src/test/java/com/berezkindv/tests/ParametrizedYandexTestWithValueSource.java
@@ -11,6 +11,8 @@ public class ParametrizedYandexTestWithValueSource {
 
     YaPage yaPage = new YaPage();
 
+    //параметризованный тест для проверки поиска Яндекса
+    //следующих значений Selenide, Allure, Junit
     @ValueSource(strings = {"Selenide", "Allure", "Junit"})
     @ParameterizedTest(name = "Тестирование общего алгоритма поиска с тестовыми данными: {0}")
     void commonSearchTest(String testData) {
