9cc3b2211f5b490c75f08410a74f2588ba4d50e5
DaymianDark <nightkonn@gmail.com>
2022-02-03 10:07:30 +0300

    comment for reviewer added

--- src/test/java/com/berezkindv/tests/TestData.java
+++ src/test/java/com/berezkindv/tests/TestData.java
@@ -5,5 +5,6 @@ public class TestData {
     public static String yaSearch = "https://yandex.ru/";
     public static String gooSearch = "https://google.com/";
     public static String bingSearch = "https://www.bing.com/";
-    //  lesson  11  homework  changes
 }
+
+/* этот комментарий добавлен для создания тестового пул реквеста в гитхабе и проверки его ревьювером */
