dec2032ac5d3745822f2fffe205de88929ba1723
berezkindv <82811285+DaymianDark@users.noreply.github.com>
2022-02-03 11:46:18 +0300

    Test branch (#3)
    * lesson 11 comment changed
    
    * comment for reviewer added

--- src/test/java/com/berezkindv/tests/TestData.java
+++ src/test/java/com/berezkindv/tests/TestData.java
@@ -6,3 +6,5 @@ public class TestData {
     public static String gooSearch = "https://google.com/";
     public static String bingSearch = "https://www.bing.com/";
 }
+
+/* этот комментарий добавлен для создания тестового пул реквеста в гитхабе и проверки его ревьювером */
